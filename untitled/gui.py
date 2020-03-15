d5e97e72c8fa1985b3dccad5f13a1f880b843fb8
WiktorWos <wiktor.wos99@gmail.com>
2020-03-15 14:35:54 +0100

    basic tkinter gui, inserting coins

---
+++ untitled/gui.py
@@ -0,0 +1,61 @@
+from vending_machine import VendingMachine, Coin, Product
+import tkinter as tk
+
+HEIGHT = 500
+WIDTH = 600
+
+
+def insert_coin(val):
+    vending_machine.insert_coin(val)
+    if vending_machine.inserted_money < 100:
+        label['text'] = "Wplaciles: " + str(vending_machine.inserted_money) + "gr"
+    else:
+        label['text'] = "Wplaciles: " + str(int(vending_machine.inserted_money / 100)) + "zl " \
+                        + str(vending_machine.inserted_money % 100) + "gr"
+
+
+root = tk.Tk()
+vending_machine = VendingMachine()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+frame_coins = tk.Frame(root, bg='#9c978a', bd=5)
+frame_coins.place(relx=0.5, rely=0.05, relwidth=0.90, relheight=0.1, anchor='n')
+
+button_1 = tk.Button(frame_coins, text="1gr", bg='grey', command=lambda: insert_coin(1))
+button_1.place(relx=0.01, rely=0.25, relwidth=0.1, relheight=0.5)
+
+button_2 = tk.Button(frame_coins, text="2gr", bg='grey', command=lambda: insert_coin(2))
+button_2.place(relx=0.12, rely=0.25, relwidth=0.1, relheight=0.5)
+
+button_5 = tk.Button(frame_coins, text="5gr", bg='grey', command=lambda: insert_coin(5))
+button_5.place(relx=0.23, rely=0.25, relwidth=0.1, relheight=0.5)
+
+button_10 = tk.Button(frame_coins, text="10gr", bg='grey', command=lambda: insert_coin(10))
+button_10.place(relx=0.34, rely=0.25, relwidth=0.1, relheight=0.5)
+
+button_20 = tk.Button(frame_coins, text="20gr", bg='grey', command=lambda: insert_coin(20))
+button_20.place(relx=0.45, rely=0.25, relwidth=0.1, relheight=0.5)
+
+button_50 = tk.Button(frame_coins, text="50gr", bg='grey', command=lambda: insert_coin(50))
+button_50.place(relx=0.56, rely=0.25, relwidth=0.1, relheight=0.5)
+
+button_100 = tk.Button(frame_coins, text="1zl", bg='grey', command=lambda: insert_coin(100))
+button_100.place(relx=0.67, rely=0.25, relwidth=0.1, relheight=0.5)
+
+button_200 = tk.Button(frame_coins, text="2zl", bg='grey', command=lambda: insert_coin(200))
+button_200.place(relx=0.78, rely=0.25, relwidth=0.1, relheight=0.5)
+
+button_500 = tk.Button(frame_coins, text="5zl", bg='grey', command=lambda: insert_coin(500))
+button_500.place(relx=0.89, rely=0.25, relwidth=0.1, relheight=0.5)
+
+frame_inserted_value = tk.Frame(root, bg='#9c978a', bd=5)
+frame_inserted_value.place(relx=0.5, rely=0.17, relwidth=0.90, relheight=0.1, anchor='n')
+
+label = tk.Label(frame_inserted_value)
+label.place(relwidth=0.4, relheight=1)
+
+textbox = tk.Entry(frame_inserted_value, font=40)
+textbox.place(relx=0.45, relwidth=0.3, relheight=1)
+root.mainloop()
--- untitled/vending_machine.py
+++ untitled/vending_machine.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class Coin:
     def __init__(self, value):
         self.value = value
@@ -24,37 +27,80 @@ class Product:
     def __str__(self) -> str:
         return "id: " + str(self.product_id) + ", name:" + str(self.name) + ", price: " + str(self.price)
 
+    def __eq__(self, other):
+        if not isinstance(other, Product):
+            return NotImplemented
+
+        return (self.product_id == other.product_id) and (self.price == other.price) and (self.name == other.name)
+
+    def __hash__(self):
+        return hash((self.product_id, self.price, self.name))
+
 
 class VendingMachine:
     def __init__(self):
-        self.coins = {Coin(10): 5,
-                      Coin(20): 5,
-                      Coin(50): 5,
-                      Coin(100): 5,
-                      Coin(200): 5,
-                      Coin(500): 5
-                      }
-        self.products = []
+        self.coins = {
+            Coin(1): 5,
+            Coin(2): 5,
+            Coin(5): 5,
+            Coin(10): 5,
+            Coin(20): 5,
+            Coin(50): 5,
+            Coin(100): 5,
+            Coin(200): 5,
+            Coin(500): 5
+        }
+        self.products = {
+            Product(30, "Coca-cola", 300): 5,
+            Product(31, "Fanta", 280): 5,
+        }
+        self.temp_coins = self.get_initial_temp_coins()
+        self.inserted_money = 0
 
-    def add_product(self, product):
-        self.products.append(product)
+    def get_initial_temp_coins(self):
+        coins = copy.deepcopy(self.coins)
+        for coin in coins:
+            coins[coin] = 0
+        return coins
+
+    def add_product(self, product, quantity):
+        self.products[product] += quantity
         print("added product: " + str(product))
 
     def add_coin(self, coin, quantity):
         self.coins[coin] += quantity
         print(self.coins[coin])
 
-    # def buy_product(self, product_id):
+    def buy_product(self, product_id):
+        paid = 0
+        for product in self.products:
+            if product_id == product.product_id:
+                while paid < product.price:
+                    coin_value = int(input("please insert a coin: \n"))
+                    paid += coin_value
+                    self.add_coin(Coin(coin_value), 1)
+                if paid > product.price:
+                    print("reszta: " + str(paid - product.price))
+                print("Kupiony produkt: " + str(product))
+                self.print_coins()
+
+    def print_coins(self):
+        for coin in self.coins:
+            print("Coin: " + str(coin) + " quantity: " + str(self.coins[coin]))
+
+    def print_temp_coins(self):
+        for coin in self.temp_coins:
+            print("Coin: " + str(coin) + " quantity: " + str(self.temp_coins[coin]))
+
+    def insert_coin(self, val):
+        coin = Coin(val)
+        self.temp_coins[coin] += 1
+        self.print_temp_coins()
+        self.inserted_money += val
 
 
-coin1 = Coin(10)
-coin2 = Coin(50)
-product1 = Product(30, 'Coca-cola', 4)
 vending_machine = VendingMachine()
-vending_machine.add_coin(coin1, 5)
-# vending_machine.add_coin(coin2)
-vending_machine.add_product(product1)
-
+vending_machine.insert_coin(1)
 
 
 
